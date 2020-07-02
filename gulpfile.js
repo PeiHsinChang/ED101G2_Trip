797929668d25e3792985c4307d531f2b15382b18
bcwu324 <bcwu324@gmail.com>
2020-07-02 10:44:59 +0800

    distance between destinations and current position

--- gulpfile.js
+++ gulpfile.js
@@ -26,49 +26,53 @@ sass.compiler = require('node-sass');
 // });
 
 //js 搬家
-gulp.task('move',function(){
-  gulp
-    .src('./dev/js/*.js') //來源
-    .pipe(gulp.dest('dest/js/')) //目的地
-})
+gulp.task('move', function() {
+    gulp
+        .src('./dev/js/*.js') //來源
+        .pipe(gulp.dest('dest/js/')) //目的地
+});
+//json搬家
+gulp.task('moveJson', function() {
+    gulp
+        .src('./dev/json/*.json') //來源
+        .pipe(gulp.dest('dest/json/')) //目的地
+});
 
 // sass 轉譯
-gulp.task("sass", function () {
-  gulp
-    .src(["./dev/sass/*.scss" , "./dev/sass/**/*.scss" ]) //來源
-    .pipe(sass().on("error", sass.logError)) //sass轉譯
-    .pipe(gulp.dest("./dest/css")); //目的地
+gulp.task("sass", function() {
+    gulp
+        .src(["./dev/sass/*.scss", "./dev/sass/**/*.scss"]) //來源
+        .pipe(sass().on("error", sass.logError)) //sass轉譯
+        .pipe(gulp.dest("./dest/css")); //目的地
 });
 /* 監聽sass */
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('sass:watch', function() {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 // html 樣板
-gulp.task("fileinclude", ["miniimg"], function () {
-  gulp
-    .src(["./dev/*.html", "./dev/*.php"])
-    .pipe(
-      fileinclude({
-        prefix: "@@",
-        basepath: "@file",
-      })
-    )
-    .pipe(gulp.dest("./dest"));
+gulp.task("fileinclude", ["miniimg"], function() {
+    gulp
+        .src(["./dev/*.html", "./dev/*.php"])
+        .pipe(
+            fileinclude({
+                prefix: "@@",
+                basepath: "@file",
+            })
+        )
+        .pipe(gulp.dest("./dest"));
 });
 
 // 壓圖
-gulp.task("miniimg", function () {
-  gulp.src("./dev/images/**")
-  .pipe(imagemin())
-  .pipe(gulp.dest("dest/images"));
+gulp.task("miniimg", function() {
+    gulp.src("./dev/images/**")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dest/images"));
 });
 
 // 同步
-gulp.task("default", function () {
-  gulp.watch(
-    ["./dev/*.html", "./dev/**/*.html", "./dev/*.php", "./dev/**/*.php","./dev/js/*.js"],
-    ["fileinclude","move","sass","sass:watch"],
-  );
+gulp.task("default", function() {
+    gulp.watch(
+        ["./dev/*.html", "./dev/**/*.html", "./dev/*.php", "./dev/**/*.php", "./dev/js/*.js"], ["fileinclude", "move", "moveJson", "sass", "sass:watch"],
+    );
 });
-
