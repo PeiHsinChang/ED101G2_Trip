8a1c797123f8c57361a8d4157e3824a920a25107
PeiHsin <zottycute@gmail.com>
2020-07-05 12:03:25 +0800

    收藏點擊+vue for

--- dev/js/member.js
+++ dev/js/member.js
@@ -1,4 +1,14 @@
+ 
 //keep click hearts 
-function keepSpotLike(){
-  console.log(this);
+function keepSpotLike(obj){
+  str = /heart.png/;
+  iskeep = str.test(obj.firstChild.src);
+  if(iskeep){
+    obj.firstChild.src = "./images/heart_r.png";
+    iskeep = false;
+  }else{
+    obj.firstChild.src = "./images/heart.png";
+    iskeep = true;
+  }
 }
+
