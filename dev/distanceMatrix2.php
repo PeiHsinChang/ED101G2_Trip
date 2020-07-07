8a27194fbf0fc109d1296d908a6afab65cd3628e
bcwu324 <bcwu324@gmail.com>
2020-07-07 11:35:32 +0800

    add attractions.json distanceMatrix2.php and create attractoin-search,attraction-item,attraction-list 3 components

---
+++ dev/distanceMatrix2.php
@@ -0,0 +1,31 @@
+<?php 
+try{
+    require_once("connect.php");
+    $sql = "select * from `attractions` where Name like '%:Name%'";
+    $attraction = $pdo->prepare($sql); 
+    $attraction->bindValue(":Name", $_POST["Name"]);
+    $attraction->execute();
+  
+    if( $attraction->rowCount()==0){ //查無此景點
+        echo "{}";
+    }else{ //成功取得景點
+      //自資料庫中取回景點資料
+        $Row = $attraction->fetch(PDO::FETCH_ASSOC);
+  
+        //將景點資訊寫入SESSION
+        $_SESSION["memNo"] = $Row["no"];
+        $_SESSION["memId"] = $Row["memId"];
+        $_SESSION["memName"] = $Row["memName"];
+        $_SESSION["email"] = $Row["email"];
+        $_SESSION["tel"] = $Row["tel"];
+  
+      $attractionInfo = array("Id"=>$_SESSION["Id"],"Name"=>$_SESSION["Name"], "Px"=>$_SESSION["Px"], "Py"=>$_SESSION["Py"], "Add"=>$_SESSION["Add"]);
+  
+      
+      echo json_encode($attractionInfo);
+      // 把關聯性陣列轉成json檔
+    }
+  }catch(PDOException $e){
+    echo $e->getMessage();
+  }
+?>
