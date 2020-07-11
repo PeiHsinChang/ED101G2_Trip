6dad5303b83961c04a08f1dc69b931049404ac33
bcwu324 <bcwu324@gmail.com>
2020-07-11 16:08:25 +0800

    insert sche from backend to db is done

--- dev/saveSchedule.php
+++ dev/saveSchedule.php
@@ -1,13 +1,13 @@
 <?php 
 
-$toBeSavedSchedule = json_decode($_POST["toBeSavedSchedule"]);
+// $toBeSavedSchedule = json_decode($_POST["toBeSavedSchedule"]);
 
 try{
     require_once("connect.php");
     $sql = "insert into sche (Mem_NO,Sche_Name,Sche_Status) values (:MemNO,:scheName,:scheStatus)";
 	$schedule = $pdo->prepare($sql);
 	
-	$schedule->bindValue(":MemNO", 'a');
+	$schedule->bindValue(":MemNO", 001);
     $schedule->bindValue(":scheName", 'b');
     $schedule->bindValue(":scheStatus", 1);
 	// $schedule->bindValue(":MemNO", $toBeSavedSchedule->MemNO);
@@ -15,9 +15,8 @@ try{
     // $schedule->bindValue(":scheStatus", $toBeSavedSchedule->scheStatus);
 	$schedule->execute();
     
-    echo '成功儲存一筆資料'
+    echo '成功儲存一筆資料';
 
-    }
   }catch(PDOException $e){
     echo $e->getMessage();
   }
