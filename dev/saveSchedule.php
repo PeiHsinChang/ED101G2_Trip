494a79880b7ec54b34b2ae7628e52c81940a41c1
bcwu324 <bcwu324@gmail.com>
2020-07-11 17:29:40 +0800

    inser sche(memno schename and schestatus) from html to sche db is done

--- dev/saveSchedule.php
+++ dev/saveSchedule.php
@@ -1,18 +1,20 @@
 <?php 
 
 // $toBeSavedSchedule = json_decode($_POST["toBeSavedSchedule"]);
+// echo $toBeSavedSchedule->MemNO;
+// echo $_POST["MemNO"];
 
 try{
     require_once("connect.php");
     $sql = "insert into sche (Mem_NO,Sche_Name,Sche_Status) values (:MemNO,:scheName,:scheStatus)";
 	$schedule = $pdo->prepare($sql);
 	
-	$schedule->bindValue(":MemNO", 001);
-    $schedule->bindValue(":scheName", 'b');
-    $schedule->bindValue(":scheStatus", 1);
-	// $schedule->bindValue(":MemNO", $toBeSavedSchedule->MemNO);
-    // $schedule->bindValue(":scheName", $toBeSavedSchedule->scheName);
-    // $schedule->bindValue(":scheStatus", $toBeSavedSchedule->scheStatus);
+	// $schedule->bindValue(":MemNO", 001);
+    // $schedule->bindValue(":scheName", 'b');
+    // $schedule->bindValue(":scheStatus", 1);
+	$schedule->bindValue(":MemNO", $_POST["MemNO"]);
+    $schedule->bindValue(":scheName", $_POST["scheName"]);
+    $schedule->bindValue(":scheStatus", $_POST["scheStatus"]);
 	$schedule->execute();
     
     echo '成功儲存一筆資料';
