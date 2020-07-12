9f6dd40b18b3655f7f1f4a7d32caa431bbb9b7d2
Ehren Chen <ehren.chen.ej@gmail.com>
2020-07-13 07:14:21 +0800

    carousel 976 below done(ajax connect)

--- dev/js/carousel.js
+++ dev/js/carousel.js
@@ -8,13 +8,24 @@ window.onload = function () {
             carouselPackage = JSON.parse(xhr.responseText);
             console.log(carouselPackage);
             // console.log(carouselPackage[4]);
+
+            //桌機版輪播
             GroupCarousel_As = carouselPackage[0];
             GroupCarousel_Bs = carouselPackage[1];
             GroupCarousel_Cs = carouselPackage[2];
             GroupCarousel_Ds = carouselPackage[3];
             GroupCarousel_Es = carouselPackage[4];
-            // // $id('GroupCarousel_deskA').innerHTML = carouselPackage[0];
 
+            //手機版輪播
+            GroupCarousel_Fs = carouselPackage[0];
+            GroupCarousel_Gs = carouselPackage[1];
+            GroupCarousel_Hs = carouselPackage[2];
+            GroupCarousel_Is = carouselPackage[3];
+            GroupCarousel_Js = carouselPackage[4];
+
+
+            // // $id('GroupCarousel_deskA').innerHTML = carouselPackage[0];
+            //桌機版輪播
             new Vue({
                 el: '#GroupCarousel_deskA',
                 data: {
@@ -58,12 +69,57 @@ window.onload = function () {
 
                 },
             });
+
+            //手機版輪播
+            new Vue({
+                el: '#GroupCarousel_deskF',
+                data: {
+                    GroupCarousel_Fs,
+                },
+                computed: {
+
+                },
+            });
+            new Vue({
+                el: '#GroupCarousel_deskG',
+                data: {
+                    GroupCarousel_Gs,
+                },
+                computed: {
+
+                },
+            });
+            new Vue({
+                el: '#GroupCarousel_deskH',
+                data: {
+                    GroupCarousel_Hs,
+                },
+                computed: {
+
+                },
+            });
+            new Vue({
+                el: '#GroupCarousel_deskI',
+                data: {
+                    GroupCarousel_Is,
+                },
+                computed: {},
+            });
+            new Vue({
+                el: '#GroupCarousel_deskJ',
+                data: {
+                    GroupCarousel_Js,
+                },
+                computed: {
+
+                },
+            });
         } else {
             alert(xhr.status);
             //如果資料傳送失敗的話顯示原因
         }
     };
-    xhr.open("post", "group_carousel.php", true);
+    xhr.open("post", "groupViewCarousel.php", true);
     xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
     xhr.send(); //因為開太快所以要送個空值回去
 };
