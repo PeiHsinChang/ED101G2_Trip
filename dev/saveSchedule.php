5fed0b0d570614153a45dfdc8f6803d222e9e167
bcwu324 <bcwu324@gmail.com>
2020-07-12 09:02:01 +0800

    insert sche_img and sche_views done

--- dev/saveSchedule.php
+++ dev/saveSchedule.php
@@ -2,11 +2,13 @@
 // insert savedScheduel to sche:
 try{
     require_once("connect.php");
-    $sql = "insert into sche (Mem_NO,Sche_Name,Sche_Status) values (:MemNO,:scheName,:scheStatus)";
+    $sql = "insert into sche (Mem_NO,Sche_Name,Sche_Img,Sche_Views,Sche_Status) values (:MemNO,:scheName,:scheImg,:scheViews,:scheStatus)";
 	$schedule = $pdo->prepare($sql);
 	
 	$schedule->bindValue(":MemNO", $_POST["MemNO"]);
     $schedule->bindValue(":scheName", $_POST["scheName"]);
+    $schedule->bindValue(":scheImg", $_POST["selectedAttractions"][0]["Picture1"]);
+    $schedule->bindValue(":scheViews", rand(1000,3000));
     $schedule->bindValue(":scheStatus", $_POST["scheStatus"]);
 	$schedule->execute();
     
