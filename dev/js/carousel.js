7efc1d671ec7696efa0f2483dba42f24ecf2256e
kellylinyy <kellylin.yuyin3@gmail.com>
2020-07-17 15:08:36 +0800

    1

--- dev/js/carousel.js
+++ dev/js/carousel.js
@@ -207,6 +207,19 @@ function groupCard() {
                 data: {
                     groupCardsAlls,
                 },
+                computed: {
+                    sortbyLike: function() {
+                        function compare(a, b) {
+                            if (a.hostlike < b.hostlike)
+                                return -1;
+                            if (a.hostlike > b.hostlike)
+                                return 1;
+                            return 0;
+                        }
+
+                        return this.sortLike.sort(compare);
+                    }
+                }
             });
 
         } else {
@@ -278,42 +291,42 @@ function group_carousel() {
 };
 
 
-function groupSortByLike() {
-    let xhr = new XMLHttpRequest();
+// function groupSortByLike() {
+//     let xhr = new XMLHttpRequest();
 
-    xhr.onload = function() {
-        if (xhr.status == 200) {
-            groupSortLikeRows = JSON.parse(xhr.responseText);
-            console.log(groupSortLikeRows[0]);
+//     xhr.onload = function() {
+//         if (xhr.status == 200) {
+//             groupSortLikeRows = JSON.parse(xhr.responseText);
+//             console.log(groupSortLikeRows[0]);
 
-            //desktop version
-            sortLike = groupSortLikeRows[0];
-            new Vue({
-                el: '#groupCardsAll',
-                data: {
-                    sortLike,
-                },
-                computed: {
-                    sortbyLike: function() {
-                        function compare(a, b) {
-                            if (a.hostlike < b.hostlike)
-                                return -1;
-                            if (a.hostlike > b.hostlike)
-                                return 1;
-                            return 0;
-                        }
+//             //desktop version
+//             sortLike = groupSortLikeRows[0];
+//             new Vue({
+//                 el: '#groupCardsAll',
+//                 data: {
+//                     sortLike,
+//                 },
+//                 computed: {
+//                     sortbyLike: function() {
+//                         function compare(a, b) {
+//                             if (a.hostlike < b.hostlike)
+//                                 return -1;
+//                             if (a.hostlike > b.hostlike)
+//                                 return 1;
+//                             return 0;
+//                         }
 
-                        return this.sortLike.sort(compare);
-                    }
-                }
-            });
+//                         return this.sortLike.sort(compare);
+//                     }
+//                 }
+//             });
 
-        } else {
-            alert(xhr.status);
-        }
-    };
-    xhr.open("post", "groupSortByLike.php", true);
-    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-    xhr.send();
+//         } else {
+//             alert(xhr.status);
+//         }
+//     };
+//     xhr.open("post", "groupSortByLike.php", true);
+//     xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+//     xhr.send();
 
-}
+// }
