817a936869ce81637638c799ee0f6e5054f90ac8
kellylinyy <kellylin.yuyin3@gmail.com>
2020-07-17 12:36:34 +0800

    1

--- dev/js/carousel.js
+++ dev/js/carousel.js
@@ -278,34 +278,42 @@ function group_carousel() {
 };
 
 
-// function groupSortByLike() {
-//     let xhr = new XMLHttpRequest();
+function groupSortByLike() {
+    let xhr = new XMLHttpRequest();
 
-//     xhr.onload = function() {
-//         if (xhr.status == 200) {
-//             groupSortLikeRows = JSON.parse(xhr.responseText);
-//             console.log(groupSortLikeRows[0]);
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            groupSortLikeRows = JSON.parse(xhr.responseText);
+            console.log(groupSortLikeRows[0]);
 
-//             //desktop version
-//             groupSorts = groupSortLikeRows[0];
-//             new Vue({
-//                 el: '#groupCardsAll',
-//                 data: {
-//                     groupSorts,
-//                 },
-//                 methods: {
-//                     sortbyLike: function() {
-//                         this.groupSorts.sort();
-//                     }
-//                 },
-//             });
+            //desktop version
+            sortLike = groupSortLikeRows[0];
+            new Vue({
+                el: '#groupCardsAll',
+                data: {
+                    sortLike,
+                },
+                computed: {
+                    sortbyLike: function() {
+                        function compare(a, b) {
+                            if (a.hostlike < b.hostlike)
+                                return -1;
+                            if (a.hostlike > b.hostlike)
+                                return 1;
+                            return 0;
+                        }
 
-//         } else {
-//             alert(xhr.status);
-//         }
-//     };
-//     xhr.open("post", "groupSortByLike.php", true);
-//     xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-//     xhr.send();
+                        return this.sortLike.sort(compare);
+                    }
+                }
+            });
 
-// }
+        } else {
+            alert(xhr.status);
+        }
+    };
+    xhr.open("post", "groupSortByLike.php", true);
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.send();
+
+}
