f07596e2096bdd04324300b3d7652f905b4584f2
kellylinyy <kellylin.yuyin3@gmail.com>
2020-07-17 09:51:09 +0800

    1

---
+++ dev/js/groupSortByLike.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            groupSortLikeRows = JSON.parse(xhr.responseText);
+            console.log(groupSortLikeRows[0]);
+
+            //desktop version
+            groupSorts = groupSortLikeRows[0];
+            new Vue({
+                el: '#groupCardsAll',
+                data: {
+                    groupSorts,
+                },
+            });
+
+        } else {
+            alert(xhr.status);
+        }
+    };
+    xhr.open("post", "groupSortByLike.php", true);
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.send();
+};
