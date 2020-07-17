28cbcecec91391e424e3bd37b664e24182dc3781
kellylinyy <kellylin.yuyin3@gmail.com>
2020-07-17 15:30:09 +0800

    1

--- dev/js/carousel.js
+++ dev/js/carousel.js
@@ -221,7 +221,6 @@ function groupCard() {
                         }
 
                         return this.groupCardsAlls.sort(compare);
-
                     }
                 }
             });
