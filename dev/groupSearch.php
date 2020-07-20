6656992e18cb8540bf3c360be3c8eec2277ce661
Ehren Chen <ehren.chen.ej@gmail.com>
2020-07-21 00:05:43 +0800

    done with group search function

--- dev/groupSearch.php
+++ dev/groupSearch.php
@@ -1,8 +1,31 @@
 <?php 
 try{
     require_once("connectMemberTable.php");
-
+    $sql="select Group_title, Group_Pic, 
+    Mem_name,Group_StartDate, Group_Deadline, 
+    round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
+    FROM grouptable g 
+    inner join membertable m
+    on g.mem_no = m.mem_no
+    where g.mem_no
+    And g.Group_title like :search_text
+    OR g.Group_Place like :search_text
+    OR g.Group_Com like :search_text";
     // $sql="select Group_title, Group_Pic, 
+    // Mem_name,Group_StartDate, Group_Deadline, 
+    // round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
+    // FROM grouptable g ,membertable m
+    // where g.mem_no = m.mem_no
+    // And g.Group_title like :search_text
+    // OR g.Group_Place like :search_text 
+    // OR g.Group_Com like :search_text";
+
+    // $sql="select * from `GroupTable` 
+    // where Group_Status=1
+    // And Group_title like :search_text
+    // OR Group_Place like :search_text 
+    // OR Group_Com like :search_text";
+    //$sql="select Group_title, Group_Pic, 
     //     Mem_name,Group_StartDate, Group_Deadline, 
     //     round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
     //     FROM grouptable g 
@@ -28,11 +51,11 @@ try{
     // AND Group_Title like :search_text
     // OR Group_Place like :search_text 
     // OR Group_Com like :search_text";
-    $sql="select * from `GroupTable` 
-    where Group_Status=1
-    AND Group_Title like :search_text
-    OR Group_Place like :search_text 
-    OR Group_Com like :search_text";
+    // $sql="select * from FROM `grouptable` g ,`membertable` m 
+    // where g.mem_no = m.mem_no
+    // AND Group_Title like :search_text
+    // OR Group_Place like :search_text 
+    // OR Group_Com like :search_text";
     //在有效團裡面要團名 集合地點 團備註包含相關字詞
     // $sql="select Group_title, Group_Pic, 
     //     Mem_name,Group_StartDate, Group_Deadline, 
