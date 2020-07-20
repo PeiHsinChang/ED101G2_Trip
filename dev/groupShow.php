4efcac7d38f9c9f483e834a9761d2363c34f92c3
kellylinyy <kellylin.yuyin3@gmail.com>
2020-07-20 10:42:46 +0800

    1

--- dev/groupShow.php
+++ dev/groupShow.php
@@ -3,7 +3,7 @@ try{
     require_once("connectMemberTable.php");
 
     //揪團卡片指令
-	$sql="select * from GroupTable";
+	$sql="select * from `GroupTable` where Group_Ppl <10";
 	$groupShow = $pdo->query($sql); 
 	$groupShow -> bindValue(":Group_NO", $_GET["Group_NO"]);
 	$groupShow -> execute();
@@ -68,13 +68,125 @@ if($groupShow->rowCount()==0){
 
 }
 ?>    
-<table align="center" class="prodTable">
-	<tr><th>書名</th><td><?=$groupShowRows["Group_NO"]?></td></tr>
-	<tr><th>團名</th><td><?=$groupShowRows["Group_title"]?></td></tr>
-	<tr><th>出發日期</th><td><?=$groupShowRows["Group_StartDate"]?></td></tr>
-	<tr><th>價格</th><td><?=$groupShowRows["price"]?></td></tr>
-	<tr><th>作者</th><td><?=$groupShowRows["author"]?></td></tr>
-	<tr><th>頁數</th><td><?=$groupShowRows["pages"]?></td></tr>
-</table>
+
+
+<div id="groupShowAll" class="tabAll">
+        <div class="tab">
+            <button class="tabs" onclick="openCity(event, 'activity')">活動說明</button>
+            <button class="tabs" onclick="openCity(event, 'chat')">留言板</button>
+        </div>
+        <div id="activity" class="tabcontent" style="display:block;">
+            <div class="actAll">
+                <aside class="actCont">
+                    <h3>活動說明</h3>
+                    <p>London is the capital city of England.</p>
+					
+					<table class="prodTable">
+						<tr><th>團號</th><td><?=$groupShowRows["Group_NO"]?></td></tr>
+						<tr><th>團名</th><td><?=$groupShowRows["Group_title"]?></td></tr>
+						<tr><th>出發日期</th><td><?=$groupShowRows["Group_StartDate"]?></td></tr>
+						<tr><th>價格</th><td><?=$groupShowRows["price"]?></td></tr>
+						<tr><th>作者</th><td><?=$groupShowRows["author"]?></td></tr>
+						<tr><th>頁數</th><td><?=$groupShowRows["pages"]?></td></tr>
+					</table>
+
+
+                </aside>
+                <aside class="btnsAct">
+                    <button onclick="alert('已報名成功\n可前往會員中心查看報名結果喔！')"><img
+                            src="#">我要報名</button>
+                    <button><img src="#">分享此團</button>
+                    <button><img src="#">收藏此團</button>
+                </aside>
+            </div>
+            <hr>
+            <!-- <div class="joinList">
+                <p>目前團員名單</p>
+                <table class="joinMem">
+                    <tr>
+                        <td><img src="./images/profile.jpg">派大星</td>
+                        <td><img src="./images/profile.jpg">愛旅遊</td>
+                        <td><img src="./images/profile.jpg">狠愛吃</td>
+                    </tr>
+                </table>
+            </div> -->
+        </div>
+        <!-- <div id="chat" class="tabcontent" style="display: none;">
+            <h3>留言板</h3>
+            <div class="chatCont">
+                <table>
+                    <tr>
+                        <td rowspan="3"><img class="commentor" src="./images/profile.jpg"></td>
+                        <td>
+                            <span>愛旅遊</span>
+                            <time>2020/05/09 10:24</time>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="replies">請問團主，花費預算大概是多少呢？</td>
+                    </tr>
+                    <tr>
+                        <td class="replies">答覆：約落在2萬～3萬左右！
+                            <time>2020/05/09 10:24</time>
+                        </td>
+                    </tr>
+                </table>
+
+
+                <table>
+                    <tr>
+                        <td rowspan="3"><img class="commentor" src="./images/profile.jpg"></td>
+                        <td>
+                            <span>愛旅遊</span>
+                            <time>2020/05/09 10:24</time>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="replies">請問團主，花費預算大概是多少呢？</td>
+                    </tr>
+                    <tr>
+                        <td class="replies">答覆：約落在2萬～3萬左右！
+                            <time>2020/05/09 10:24</time>
+                        </td>
+                    </tr>
+                </table>
+
+                <table>
+                    <tr>
+                        <td rowspan="3"><img class="commentor" src="./images/profile.jpg"></td>
+                        <td>
+                            <span>愛旅遊</span>
+                            <time>2020/05/09 10:24</time>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="replies">請問團主，花費預算大概是多少呢？</td>
+                    </tr>
+                    <tr>
+                        <td class="replies">答覆：約落在2萬～3萬左右！
+                            <time>2020/05/09 10:24</time>
+                        </td>
+                    </tr>
+                </table>
+
+                <table>
+                    <tr>
+                        <td rowspan="3"><img class="commentor" src="./images/profile.jpg"></td>
+                        <td>
+                            <span>中央劉德華</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="text"><input class="btnSend" type="submit" value="">
+                        </td>
+                    </tr>
+                </table>
+
+            </div>
+        </div> -->
+
+    </div>
+
 </body>
 </html>
