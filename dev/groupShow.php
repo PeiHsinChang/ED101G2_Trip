bfbe92f1422efc0a0905b5c0313ef290a825862d
kellylinyy <kellylin.yuyin3@gmail.com>
2020-07-20 11:13:47 +0800

    1

--- dev/groupShow.php
+++ dev/groupShow.php
@@ -3,39 +3,39 @@ try{
     require_once("connectMemberTable.php");
 
     //揪團卡片指令
-	$sql="select * from GroupTable where Group_NO:Group_NO";
+	$sql="select * from GroupTable";
 	$groupShow = $pdo->query($sql); 
-	$groupShow -> bindValue(":Group_NO", $_GET["Group_NO"]);
+	// $groupShow -> bindValue(":Group_NO", $_GET["Group_NO"]);
 	$groupShow -> execute();
 	// $groupShowInfo=$groupShow->fetchAll(PDO::FETCH_ASSOC);
 
 
 
-    //卡片們所需要資料
-    // $groupShowCards=array();
-    // if($groupShow -> rowCount()==0){
-    //     echo "{}";
-    // }else{
-    //     $groupShowInfo=array();
-    //     while($groupShowRows = $groupShow->fetch(PDO::FETCH_ASSOC)){
-    //     $groupShowInfo[] = array(
-    //             "Group_title"=>$groupShowRows["Group_title"],
-	// 			"Group_StartDate"=>$groupShowRows["Group_StartDate"],
-	// 			"Group_EndDate"=>$groupShowRows["Group_EndDate"],
-	// 			"Group_Deadline"=>$groupShowRows["Group_Deadline"],
-	// 			"Group_Ppl"=>$groupShowRows["Group_Ppl"],
-	// 			"Group_Sex"=>$groupShowRows["Group_Sex"],
-	// 			"Group_Age"=>$groupShowRows["Group_Age"],
-	// 			"Group_Place"=>$groupShowRows["Group_Place"],
-	// 			"Group_Com"=>$groupShowRows["Group_Com"],
-	// 			"Group_Pic"=>$groupShowRows["Group_Pic"],
-    //         );	
-    //     }   
-    // }
+    卡片們所需要資料
+    $groupShowCards=array();
+    if($groupShow -> rowCount()==0){
+        echo "{}";
+    }else{
+        $groupShowInfo=array();
+        while($groupShowRows = $groupShow->fetch(PDO::FETCH_ASSOC)){
+        $groupShowInfo[] = array(
+                "Group_title"=>$groupShowRows["Group_title"],
+				"Group_StartDate"=>$groupShowRows["Group_StartDate"],
+				"Group_EndDate"=>$groupShowRows["Group_EndDate"],
+				"Group_Deadline"=>$groupShowRows["Group_Deadline"],
+				"Group_Ppl"=>$groupShowRows["Group_Ppl"],
+				"Group_Sex"=>$groupShowRows["Group_Sex"],
+				"Group_Age"=>$groupShowRows["Group_Age"],
+				"Group_Place"=>$groupShowRows["Group_Place"],
+				"Group_Com"=>$groupShowRows["Group_Com"],
+				"Group_Pic"=>$groupShowRows["Group_Pic"],
+            );	
+        }   
+    }
 
-    //串接資料
-    // array_push($groupShowCards,$groupShowInfo);
-    // echo json_encode($groupShowCards,JSON_UNESCAPED_UNICODE);
+    串接資料
+    array_push($groupShowCards,$groupShowInfo);
+    echo json_encode($groupShowCards,JSON_UNESCAPED_UNICODE);
 
 }catch(PDOException $e){
  echo "錯誤行號:", $e->getLine(),"<br>";
