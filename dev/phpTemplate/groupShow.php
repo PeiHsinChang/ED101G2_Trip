0656ca4e2397abbf85928752f7a105fbf504849e
kellylinyy <kellylin.yuyin3@gmail.com>
2020-07-20 16:30:12 +0800

    1

--- dev/phpTemplate/groupShow.php
+++ dev/phpTemplate/groupShow.php
@@ -10,7 +10,7 @@ try {
 	$groupShow = $pdo->prepare($sql_g);
 	$groupShow->bindValue(":Group_NO", $_GET["Group_NO"]);
     $groupShow->execute();
-    // $groupShowInfo = $groupShow->fetch(PDO::FETCH_ASSOC);
+    $groupShowInfo = $groupShow->fetch(PDO::FETCH_ASSOC);
     //echo $groupShowInfo["Mem_NO"]==$_SESSION["Mem_NO"]?'是團主':'不是團主';
 } catch (PDOException $e) {
 	// echo "系統暫時無法提供服務, 請通知系統維護人員<br>";
@@ -27,7 +27,7 @@ try {
 </div>
 
 
-<h5><?php print_r( $groupShowInfo);?></h5>
+<h5><?php print_r($groupShowInfo);?></h5>
 <div class="container">
     <div class="openNewGroup col-4 col-l-2 col-md-2 col-lg-2">
         <a  onclick="openNewGroup();">
@@ -52,15 +52,9 @@ try {
 </style>
 </head>
 <body>
-<?php 
-if($groupShowInfo->rowCount()==0){
-	echo "<center>查無此商品資料</center>";
-}else{
-	$groupShowInfo = $groupShow->fetch(PDO::FETCH_ASSOC);
-}
-?>    
+  
 <table align="center" class="prodTable">
-	<tr><th>書號</th><td><?=$groupShowInfo["psn"]?></td></tr>
+	<tr><th>團名</th><td><?=$groupShowInfo["Group_title"]?></td></tr>
 	<tr><th>書名</th><td><?=$groupShowInfo["pname"]?></td></tr>
 	<tr><th>價格</th><td><?=$groupShowInfo["price"]?></td></tr>
 	<tr><th>作者</th><td><?=$groupShowInfo["author"]?></td></tr>
