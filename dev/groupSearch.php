75ca7e8b920d827c7bdd1083e50b5e82cdf2dc22
Ehren Chen <ehren.chen.ej@gmail.com>
2020-07-21 00:13:55 +0800

    clean the unuseful code

--- dev/groupSearch.php
+++ dev/groupSearch.php
@@ -1,6 +1,7 @@
 <?php 
 try{
     require_once("connectMemberTable.php");
+    //把資訊塞進去sql裡面
     $sql="select Group_title, Group_Pic, 
     Mem_name,Group_StartDate, Group_Deadline, 
     round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
@@ -11,57 +12,7 @@ try{
     And g.Group_title like :search_text
     OR g.Group_Place like :search_text
     OR g.Group_Com like :search_text";
-    // $sql="select Group_title, Group_Pic, 
-    // Mem_name,Group_StartDate, Group_Deadline, 
-    // round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
-    // FROM grouptable g ,membertable m
-    // where g.mem_no = m.mem_no
-    // And g.Group_title like :search_text
-    // OR g.Group_Place like :search_text 
-    // OR g.Group_Com like :search_text";
-
-    // $sql="select * from `GroupTable` 
-    // where Group_Status=1
-    // And Group_title like :search_text
-    // OR Group_Place like :search_text 
-    // OR Group_Com like :search_text";
-    //$sql="select Group_title, Group_Pic, 
-    //     Mem_name,Group_StartDate, Group_Deadline, 
-    //     round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
-    //     FROM grouptable g 
-    //     where g.mem_no = m.mem_no
-    //     AND g.Group_Title like :search_text
-    //     OR g.Group_Place like :search_text 
-    //     OR g.Group_Com like :search_text";
-    //把資訊塞進去sql裡面
-    // $sql="select Group_title, Group_Pic, 
-    // Mem_name,Group_StartDate, Group_Deadline, 
-    // round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
-    // FROM grouptable g ,membertable m
-    // WHERE Group_Status=1
-    // AND Group_Title like :search_text
-    // OR Group_Place like :search_text 
-    // OR Group_Com like :search_text";
-
-    // $sql="select select Group_title, Group_Pic, 
-    // Mem_name,Group_StartDate, Group_Deadline, 
-    // round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
-    // from `GroupTable` 
-    // where Group_Status=1
-    // AND Group_Title like :search_text
-    // OR Group_Place like :search_text 
-    // OR Group_Com like :search_text";
-    // $sql="select * from FROM `grouptable` g ,`membertable` m 
-    // where g.mem_no = m.mem_no
-    // AND Group_Title like :search_text
-    // OR Group_Place like :search_text 
-    // OR Group_Com like :search_text";
     //在有效團裡面要團名 集合地點 團備註包含相關字詞
-    // $sql="select Group_title, Group_Pic, 
-    //     Mem_name,Group_StartDate, Group_Deadline, 
-    //     round(Mem_LikeAmount/Mem_LikeSum) hostlike,Group_NO
-    //     FROM grouptable g ,membertable m
-    //     where g.mem_no = m.mem_no";
     $result = $pdo->prepare($sql);
     $result->bindValue(":search_text",'%'.$_POST['search_text'].'%');
     $result->execute();
@@ -71,11 +22,6 @@ try{
         $searchResultRows=$result->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($searchResultRows);
     }
-    // $searchResultRows=$result->fetchAll(PDO::FETCH_ASSOC);
-    // print_r($carouselGroupRows);
-    // die;
-    
-    // echo json_encode($searchResultRows,JSON_UNESCAPED_UNICODE);
     
 }catch(PDOException $e){
     echo "錯誤行號：",$e->getLine(),"<br>";
