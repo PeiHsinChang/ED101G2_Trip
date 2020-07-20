f1933e3b73e35251a9e73e32d326083012bd669f
kellylinyy <kellylin.yuyin3@gmail.com>
2020-07-20 16:47:25 +0800

    1

--- dev/phpTemplate/groupShow.php
+++ dev/phpTemplate/groupShow.php
@@ -11,6 +11,31 @@ try {
 	$groupShow->bindValue(":Group_NO", $_GET["Group_NO"]);
     $groupShow->execute();
     $groupShowInfo = $groupShow->fetch(PDO::FETCH_ASSOC);
+
+
+    // $groupShowCards=array();
+    // if($groupShow -> rowCount()==0){
+    //     echo "{}";
+    // }else{
+    //     $groupShowInfo=array();
+    //     while($groupShowsRows = $groupShow->fetch(PDO::FETCH_ASSOC)){
+    //     $groupShowInfo[] = array(
+    //             "Group_NO"=>$groupShowsRows["Group_NO"],
+    //             "Group_title"=>$groupShowsRows["Group_title"],
+    //             "Group_Pic"=>$groupShowsRows["Group_Pic"],
+    //             "Mem_name"=>$groupShowsRows["Mem_name"],
+    //             "Group_StartDate"=>$groupShowsRows["Group_StartDate"],
+    //             "Group_Deadline"=>$groupShowsRows["Group_Deadline"],
+    //             "hostlike"=>$groupShowsRows["hostlike"],
+    //         );	
+    //     }   
+       
+        
+    // }
+
+    //串接資料
+    // array_push($groupShowCards,$groupShowInfo);
+    // echo json_encode($groupShowCards,JSON_UNESCAPED_UNICODE);
     //echo $groupShowInfo["Mem_NO"]==$_SESSION["Mem_NO"]?'是團主':'不是團主';
 } catch (PDOException $e) {
 	// echo "系統暫時無法提供服務, 請通知系統維護人員<br>";
@@ -54,16 +79,16 @@ try {
 <body>
   
 <table  class="prodTable">
-	<tr><th>團名</th><td><?=$groupShowInfo["Group_title"]?></td></tr>
-	<tr><th>出發日期</th><td><?=$groupShowInfo["Group_StartDate"]?></td></tr>
-	<tr><th>結束日期</th><td><?=$groupShowInfo["Group_EndDate"]?></td></tr>
-	<tr><th>報名截止</th><td><?=$groupShowInfo["Group_EndDate"]?></td></tr>
-    <tr><th>人數限制</th><td><?=$groupShowInfo["Group_Ppl"]?></td></tr>
-    <tr><th>性別限制</th><td><?=$groupShowInfo["Group_Sex"]?></td></tr>
-    <tr><th>年齡限制</th><td><?=$groupShowInfo["Group_Age"]?></td></tr>
-    <tr><th>團費</th><td><?=$groupShowInfo["Group_Fee"]?></td></tr>
-    <tr><th>集合地點</th><td><?=$groupShowInfo["Group_Place"]?></td></tr>
-    <tr><th>備註</th><td><?=$groupShowInfo["Group_Com"]?></td></tr>
+	<tr><th>團名</th><td><?=$groupShowsRows["Group_title"]?></td></tr>
+	<tr><th>出發日期</th><td><?=$groupShowsRows["Group_StartDate"]?></td></tr>
+	<tr><th>結束日期</th><td><?=$groupShowsRows["Group_EndDate"]?></td></tr>
+	<tr><th>報名截止</th><td><?=$groupShowsRows["Group_EndDate"]?></td></tr>
+    <tr><th>人數限制</th><td><?=$groupShowsRows["Group_Ppl"]?></td></tr>
+    <tr><th>性別限制</th><td><?=$groupShowsRows["Group_Sex"]?></td></tr>
+    <tr><th>年齡限制</th><td><?=$groupShowsRows["Group_Age"]?></td></tr>
+    <tr><th>團費</th><td><?=$groupShowsRows["Group_Fee"]?></td></tr>
+    <tr><th>集合地點</th><td><?=$groupShowsRows["Group_Place"]?></td></tr>
+    <tr><th>備註</th><td><?=$groupShowsRows["Group_Com"]?></td></tr>
 </table>
 </body>
 </html>
